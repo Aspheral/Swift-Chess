73258bbf346c6b3dbd9c23727a79a82d913f477f
Aspheral <aspheralmusic@gmail.com>
2026-09-16 01:58:59 -0500

    Export Swift benchmark suite

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./chess/board";
 export * from "./chess/game";
 export * from "./chess/engine";
 export { PvsSwiftEngine as SwiftEnginePVS } from "./chess/pvs";
+export * from "./chess/benchmark";
 export * from "./chess/see";
 export * from "./chess/tactics";
